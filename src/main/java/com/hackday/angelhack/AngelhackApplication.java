45a92f2528bcb285b548c39b3b48efe6de3dc4b7
hahava <hahava5696@gmail.com>
2020-07-14 09:15:50 +0900

    Test user get / set

--- src/main/java/com/hackday/angelhack/AngelhackApplication.java
+++ src/main/java/com/hackday/angelhack/AngelhackApplication.java
@@ -2,8 +2,10 @@ package com.hackday.angelhack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class AngelhackApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/hackday/angelhack/user/UserRepositoryTest.java
+++ src/test/java/com/hackday/angelhack/user/UserRepositoryTest.java
@@ -1,4 +1,30 @@
 package com.hackday.angelhack.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void userSaveTest() {
+        UserAuth userAuth = new UserAuth();
+        userAuth.setEmail("hahava@naver.com");
+        userAuth.setPw("{noop}12345");
+        userAuth.setRole(ROLE.PROJECT_MANAGER);
+        userRepository.save(userAuth);
+    }
+
+    @Test
+    public void getUserTest() {
+        UserAuth mockUserAuth = userRepository.findByEmail("hahava@naer.com");
+        assertEquals(mockUserAuth.getRole(), ROLE.PROJECT_MANAGER);
+    }
+
 }
