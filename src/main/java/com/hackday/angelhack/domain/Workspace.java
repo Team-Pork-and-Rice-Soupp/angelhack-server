d98349941282d9457da8145ca9774b3bc7fbc413
yoonjaesong <thddbswo14@gmail.com>
2020-07-14 15:04:01 +0900

    Implement Workspace domain

---
+++ src/main/java/com/hackday/angelhack/domain/Workspace.java
@@ -0,0 +1,33 @@
+package com.hackday.angelhack.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "workspaces")
+public class Workspace extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    private String description;
+
+    @Builder
+    public Workspace(Long userId, String title, String description) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+}
