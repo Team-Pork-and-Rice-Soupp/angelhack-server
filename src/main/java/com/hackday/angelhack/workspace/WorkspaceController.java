5051807674c8300c3a2456be55128da10aa79b82
yoonjaesong <thddbswo14@gmail.com>
2020-07-17 17:40:05 +0900

    Add Websocket Config

--- src/main/java/com/hackday/angelhack/workspace/WorkspaceController.java
+++ src/main/java/com/hackday/angelhack/workspace/WorkspaceController.java
@@ -8,9 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
