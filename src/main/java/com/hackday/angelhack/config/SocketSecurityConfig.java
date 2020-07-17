1adb8c953b520824fde687727e3d357dcf04969c
yoonjaesong <thddbswo14@gmail.com>
2020-07-17 18:49:33 +0900

    Add web socket security

---
+++ src/main/java/com/hackday/angelhack/config/SocketSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.hackday.angelhack.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class SocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpDestMatchers("/websocket/endpoint", "/websocket/subscribe")
+                .authenticated()
+                .anyMessage()
+                .authenticated();
+    }
+}
